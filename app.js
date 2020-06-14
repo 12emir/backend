86d7682f476c0bc8496875db64b5e80202763f2d
Kozakiewicz <emir.alobedi@gmail.com>
2020-06-15 01:04:23 +0200

    3

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/createtable", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Homepage");
+  res.send("Homepageeee !!!");
 });
 db.connect((err) => {
   if (err) throw err;
